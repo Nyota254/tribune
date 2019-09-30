502f43427262454b0a176cbf4ee517d8d32d27da
Nyota254 <knyota66@gmail.com>
2019-09-30 12:48:38 +0300

    add external templates and show template inheritance

--- news/views.py
+++ news/views.py
@@ -1,35 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse,Http404
 import datetime as dt
 
 # Create your views here.
 def welcome(request):
-    return HttpResponse('Welcome To Moringa Tribune')
+    return render(request,'welcome.html')
 
 
 def news_of_day(request):
     date = dt.date.today()
 
     #Function to convert date object to find exact dates
-    day = convert_dates(date)
-    html = f'''
-            <html>
-                <body>
-                    <h1>News for {day} {date.day}-{date.month}-{date.year}</h1>
-                </body>
-            </html>
-            '''
-    return HttpResponse(html)
+    # day = convert_dates(date)
+    # html = f'''
+    #         <html>
+    #             <body>
+    #                 <h1>News for {day} {date.day}-{date.month}-{date.year}</h1>
+    #             </body>
+    #         </html>
+    #         '''
+    return render(request,'all-news/today-news.html',{"date":date})
 
-def convert_dates(dates):
-    #function that gets the weekday number for dates
-    day_number = dt.date.weekday(dates)
+# def convert_dates(dates):
+#     ##function that gets the weekday number for dates
+#     day_number = dt.date.weekday(dates)
 
-    days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
+#     days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
 
-    #Returning the actual day of the week
-    day = days[day_number]
-    return day
+#     ##Returning the actual day of the week
+#     day = days[day_number]
+#     return day
 
 def past_days_news(request,past_date):
     try:
@@ -38,14 +38,18 @@ def past_days_news(request,past_date):
     except ValueError:
         #Raise 404 Once value error is thrown
         raise Http404()
+        assert False
 
-    day = convert_dates(date)
+    # day = convert_dates(date)
 
-    html = f'''
-            <html>
-                <body>
-                    <h1>News for {day} {date.day}-{date.month}-{date.year}</h1>
-                </body>
-            </html>
-            '''
-    return HttpResponse(html)
+    # html = f'''
+    #         <html>
+    #             <body>
+    #                 <h1>News for {day} {date.day}-{date.month}-{date.year}</h1>
+    #             </body>
+    #         </html>
+    #         '''
+
+    if date == dt.date.today():
+        return redirect(news_of_day)
+    return render(request,'all-news/past-news.html',{"date":date})
