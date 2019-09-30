30618ff2e9ba35c4aad213720c5173cbe2dc69e5
Nyota254 <knyota66@gmail.com>
2019-09-30 17:07:25 +0300

    add tests to the project

--- news/models.py
+++ news/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+class Editor(models.Model):
+    '''
+    Contains the details of the person writing the news article
+    '''
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.first_name
+
+    def save_editor(self):
+        self.save()
+
+    def delete_editor(self):
+        self.delete()
+
+class tags(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+class Article(models.Model):
+    '''
+    contains articles written
+    '''
+    title = models.CharField(max_length=60)
+    post = models.TextField()
+    editor = models.ForeignKey(Editor)
+    tags = models.ManyToManyField(tags)
+    pub_date = models.DateTimeField(auto_now_add=True)
--- news/tests.py
+++ news/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from .models import Editor,Article,tags
 
-# Create your tests here.
+
+class EditorTestClass(TestCase):
+    #Set up method
+    def setUp(self):
+        self.kevin = Editor(first_name='Kevin',last_name='Nyota',email='kevin@gmail.com')
+
+    #Testing Instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.kevin,Editor))
+
+    #Testing the save method
+    def test_save_method(self):
+        self.kevin.save_editor()
+        editors = Editor.objects.all()
+        self.assertTrue(len(editors)>0)
+
+    #Testing the delete method
+    def test_delete_method(self):
+        self.jescah = Editor(first_name='jescah',last_name='M',email='jesca@gmail.com')
+        self.jescah.save_editor()
+        self.kevin.save_editor()
+        self.kevin.delete_editor()
+        editors = Editor.objects.all()
+        self.assertEqual(len(editors),1)
