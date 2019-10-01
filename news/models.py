441af7a8f53ba2f2da8472b19d9237d271eadc5e
Nyota254 <knyota66@gmail.com>
2019-10-01 16:13:09 +0300

    add a searchform and image upload capability

--- news/models.py
+++ news/models.py
@@ -35,6 +35,7 @@ class Article(models.Model):
     editor = models.ForeignKey(Editor)
     tags = models.ManyToManyField(tag)
     pub_date = models.DateTimeField(auto_now_add=True)
+    article_image = models.ImageField(upload_to='articles/')
 
     @classmethod
     def todays_news(cls):
@@ -45,4 +46,9 @@ class Article(models.Model):
     @classmethod
     def days_news(cls,date):
         news = cls.objects.filter(pub_date__date = date)
+        return news
+
+    @classmethod
+    def search_by_title(cls,search_term):
+        news = cls.objects.filter(title__icontains=search_term)
         return news
